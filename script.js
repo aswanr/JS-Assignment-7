75456e12818b55495c23b59c42767c57e93f9e81
Aswan R <aswan.r@coddletech.com>
2024-11-19 09:53:26 +0530

    script updated

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var obj = new XMLHttpRequest();
 obj.open('GET', './json/new.json');
 obj.onload = function () {
@@ -8,19 +7,8 @@ obj.onload = function () {
     const orginaldate = new Date(globalData.metadata.creationDate);
     const options = { month: 'short', day: 'numeric', year: 'numeric', weekday: 'short', };
     const formatedate = orginaldate.toLocaleDateString('en-US', options);
-=======
-$.ajax({
-    type: "get",
-    url: "./json/new.json",
-    success: function (response) {
-        const obj = JSON.stringify(response);
-        const globalData = JSON.parse(obj);
-        const head = document.getElementById("dicreptions");
-        head.innerHTML = `<h3>${globalData.description}</h3>`;
 
->>>>>>> 038b4b30793356dca0ba29fe9abf9648726771db
-    // 3)Create a simple form (also add validation) to add a new items to the items array and display it immediately in the card section.
-    // Cards 
+    // cards
     const cards = document.getElementById("cards");
     const k = globalData.items;
     k.forEach((element) => {
@@ -34,26 +22,8 @@ $.ajax({
         cards.appendChild(card);
     });
 
-<<<<<<< HEAD
-    // Filter button 
-    const filt = document.createElement('button');
-    const input = document.createElement('input');
-    filt.id = "filter";
-    input.id = "inputvalue"
-    filt.textContent = "filter"
-    head.appendChild(filt);
-    head.appendChild(input);
-    const filterbutton = document.getElementById('filter');
-    filterbutton.addEventListener('click', filterfun);
-    function filterfun() {
-        const inputvalue = document.getElementById('inputvalue').value;
-        const fill = document.getElementById('filtediteams');
-        fill.innerHTML = "";
-        k.forEach(element => {
-            if (Number(inputvalue) >= element.price) {
-=======
         // 1)Write a JavaScript function to filter items in the array based on price (e.g., show only items over $500).
-        // Filter button 
+        // Filter button
         const filt = document.createElement('button');
         const input = document.createElement('input');
         filt.id = "filter";
@@ -79,37 +49,7 @@ $.ajax({
             });
         }
 
-        // 2)Create a function to sort the array of items by name or price in ascending or descending order.
-        // Sorting Acending order
-        const sort = document.getElementById("sorting");
-        const sortbtn = document.createElement('button');
-        sortbtn.textContent = "Acending order";
-        sort.appendChild(sortbtn);
-
-        function sortedbyname(iteam) {
-           return iteam.sort((a, b) => a.name.localeCompare(b.name));
-        }
-
-        sortbtn.addEventListener('click', function () {
-            const sortediteam = sortedbyname(k);
-            sortedcard(sortediteam);
-        });
-
-        function sortedcard(y) {
-            const sorted = document.getElementById('cards'); 
-            cards.innerHTML="";
-            y.forEach(item => {
->>>>>>> 038b4b30793356dca0ba29fe9abf9648726771db
-                const card = document.createElement("div");
-                card.className = "card";
-                card.innerHTML = `<h2> ${element.name} </h2>
-                    <p>Price : ${element.price} </p>
-                    <p> ${element.description} </p>`;
-                fill.appendChild(card);
-            }
-        });
-    }
-
+    // 2)Create a function to sort the array of items by name or price in ascending or descending order.
     // Sorting Acending order
     const sort = document.getElementById("sorting");
     const sortbtn = document.createElement('button');
@@ -137,6 +77,7 @@ $.ajax({
             sorted.appendChild(card);
         });
     }
+
     // Sorting in decending
     const dec = document.getElementById("sorting");
     const sortbtn2 = document.createElement('button');
@@ -164,6 +105,8 @@ $.ajax({
             sorteding.appendChild(card);
         });
     }
+
+    // 3)Create a simple form (also add validation) to add a new items to the items array and display it immediately in the card section.
     // Form validation
     const form = document.getElementById("form");
     form.addEventListener('submit', function (event) {
